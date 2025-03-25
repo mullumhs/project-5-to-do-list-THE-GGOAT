380cdc38410b4401c65bd17dc1a3b8240f0e9363
THE-GGOAT <147569918+THE-GGOAT@users.noreply.github.com>
2025-03-25 13:35:33 +1100

    finished

--- to-do-list.py
+++ to-do-list.py
@@ -24,17 +24,19 @@ def add_task():
 
 def remove_task():
     print_list()
-    remove = get_number("Remove a task: ")
     try: 
+        remove = get_number("Remove a task: ")
         task_list.pop(remove)
-    except IndexError:
+    except:
+       ("ENter vaild choice") 
 
 def print_list():
     for task in task_list:
         print(task)
 
 def menu():
-        print("1. Add to list\n2. Remove from list\n3. Print list")
+        print_list()
+        print("1. Add to list\n2. Remove from list\n3. Print list\n4. Edit task")
         while True: 
             choice = input("What function would you like to run: ")
             
@@ -46,8 +48,21 @@ def menu():
 
             elif choice == "3":
                 print_list()
+            
+            elif choice == "4":
+                edit_task()
+
+def edit_task():
+    print_list()
+    var = input("what task to edit: ")
+    var_num = task_list.index(var)
+    change = input("Enter edited task:")
+    try:
+        task_list[var_num] = change
+        print_list()
+    except:
+        print("Invaild Input, try again")
 
 
 while True:
     menu()
-
